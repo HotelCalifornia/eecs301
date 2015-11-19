97046792213a2fd3c386c27851899bae9d0a1741
Alex (a.k.a kintama.tar.gz) <akb.sbc@gmail.com>
2015-11-19 11:40:50 -0600

    Create convert_ir_dms.py

---
+++ src/eecs_grp_e/src/convert_ir_dms.py
@@ -0,0 +1,15 @@
+# measure out 15"
+# take IR/DMS readings
+# record
+# repeat like 10 times
+# calculate ratios for each datapoint
+# average ratios
+# use ratios in conversion functions
+
+
+def convert_dms_ins(dms_val):
+    return dms_val / 1  # change 1 ==> conversion ratio found above
+
+
+def convert_ir_ins(ir_val):
+    return ir_val / 1  # change 1 ==> conversion ratio found above
