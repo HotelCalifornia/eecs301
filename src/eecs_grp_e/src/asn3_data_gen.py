472846b4b79ec8b9c4998857d6c487d172574d1c
Alex (a.k.a kintama.tar.gz) <akb.sbc@gmail.com>
2015-11-19 11:14:11 -0600

    Update asn3_data_gen.py

--- src/eecs_grp_e/src/asn3_data_gen.py
+++ src/eecs_grp_e/src/asn3_data_gen.py
@@ -2,11 +2,13 @@ import csv
 
 
 def generate_data(density=30):
-	'''Generate a training set of data'''
-	with open('data.csv', 'wb') as f:
-		writer = csv.writer(f, delimiter=':')
-		for x0 in xrange(density, -density, step=-1):
-			for x1 in xrange(density, -density, step=-1):
-				writer.writerow([x0, x1, (x0 * -1, x1 * -1)])
+    """Generate a training set of data
+    :param density: Specifies how many data points will be created ((density*2)**2) [default: 30]
+    """
+    with open('data.csv', 'wb') as f:
+        writer = csv.writer(f, delimiter=':')
+        for x0 in xrange(density, -density, -1):
+            for x1 in xrange(density, -density, -1):
+                writer.writerow([x0, x1, (x0 * -1, x1 * -1)])
 
-# generate_data()
+generate_data(15)
